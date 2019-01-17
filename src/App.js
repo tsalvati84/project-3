f706e922364a9d6a8ff89987fd900f610357fba7
Tony Salvati <tsal2184@gmail.com>
2019-01-16 18:53:27 -0700

    updated layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
+import SignIn from "./pages/SignIn.js";
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <Router>
       <div>
         <Nav />
-        <Route exact path="/" component={Books} />
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/SignIn" component={SignIn} />
         <Route exact path="/Books" component={Books} />
         <Route exact path="/Nav" component={Nav} />
 
